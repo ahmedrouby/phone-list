57b885bddc94516be40c959afc597b9c98a32cef
Ahmed Rouby <ahmedrouby96@gmail.com>
2023-04-03 22:39:04 +0200

    Enhanced source code

--- task1.py
+++ task1.py
@@ -3,7 +3,11 @@ while True:
     name = input("Enter your name:\n")
     number = input ("Enter your phone number:\n")
     number_type = input ("Enter your phone number type:\n")
-    contact = name +", "+ number +", "+ number_type
+    contact = name +","+ number +","+ number_type
     contacts_list.append (contact)
-    for i in range(len(contacts_list)):
-        print (contacts_list[i])
+    print ("Contacts:\n")
+    for i in (contacts_list):
+        print (i)
+    print ("\n")
+
+
